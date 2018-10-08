2f3a04d954f666fb4e358b23f7e70453cf170d36
John A Stevenson <jostev@bgs.ac.uk>
2018-10-08 16:29:37 +0100

    Add plot_nme_files.py and instructions

---
+++ ash-model-plotting/plot_name_files.py
@@ -0,0 +1,102 @@
+# coding: utf-8
+import argparse
+import glob
+import logging
+import os
+
+import iris
+import iris.plot as iplt
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import numpy as np
+
+# Monkey patch GeoAxes to fix Matplotlib v3.0.0 - related bug
+from matplotlib.axes import Axes
+from cartopy.mpl.geoaxes import GeoAxes
+GeoAxes._pcolormesh_patched = Axes.pcolormesh
+
+logging.basicConfig(level=logging.INFO)
+
+
+def plot_name_files(source_dir, prefix):
+    """
+    Read the files named 'source_dir/prefix*' and generate plots. Files are
+    saved with `plots` dir.
+    :param source_dir: str, path to directory containing files
+    :param prefix: str, common prefix of files to parse
+    """
+    for source_file in glob.glob(os.path.join(source_dir, prefix) + '*'):
+        plot_levels(source_file)
+
+
+def plot_levels(source_file):
+    """
+    Plot a single NAME file
+    :param source_file:
+    """
+    # Load data
+    cubes = iris.load(source_file)
+    # TODO: fix this hack that skips ground-level cube (try concatenating?)
+    cube = cubes[1]
+
+    # Mask out data below threshold
+    cube.data = np.ma.masked_less(cube.data, 1e-8)
+
+    # Prepare plot directory
+    plot_dir = 'plots'
+    if not os.path.isdir(plot_dir):
+        os.mkdir(plot_dir)
+
+    # Plot
+    levels = cube.coord('altitude')
+    for i, level in enumerate(levels):
+        # Prepare plot directory for given level
+        level_name = f'{int(level.points[0]):05d}'  # Get level as text string
+        level_plot_dir = os.path.join(plot_dir, level_name)
+        if not os.path.isdir(level_plot_dir):
+            os.mkdir(level_plot_dir)
+
+        # Plot map for level
+        fig, title = plot_level(cube, level, i)
+        fig.savefig(os.path.join(plot_dir, level_name, title),
+                    bbox_inches='tight')
+        plt.close(fig)
+
+
+def plot_level(cube, level, idx):
+    """
+    Plot a map of an individual level from a data cube
+    :param cube: iris Cube with data
+    :param level: int, level value
+    :param idx: int, index for given level
+    :return fig, name: Matplotlib figure with plot and str with name
+    """
+    # Plot data
+    fig = plt.figure()
+    mesh_plot = iplt.pcolormesh(cube[idx, :, :])
+    ax = plt.gca()
+    ax.coastlines(resolution='50m')
+    ax.grid(True)
+    fig.colorbar(mesh_plot, orientation='horizontal')
+
+    # Get and set title
+    timestamp = cube.coord('time')
+    title = "{title}_{quantity}_{level:05d}_{timestamp}.png".format(
+        title=cube.attributes.get('Title').replace(' ', '_'),
+        quantity=cube.attributes.get('Quantity').replace(' ', '_'),
+        level=int(level.points[0]),
+        timestamp=timestamp.units.num2date(timestamp.points[0]).strftime('%Y%m%d%H%M%S')
+    )
+    logging.info(title)
+    ax.set_title(title)
+
+    return fig, title
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Generate plots from directory of NAME data')
+    parser.add_argument('source_dir', help="Path to directory containing NAME output")
+    parser.add_argument('prefix', help="Filename prefix e.g. Air_Conc_grid_")
+    args = parser.parse_args()
+    plot_name_files(args.source_dir, args.prefix)
