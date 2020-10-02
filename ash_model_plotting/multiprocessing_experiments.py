8cb4869c8be9ec665eda9ae64d007342af3dae2d
John A Stevenson <jostev@bgs.ac.uk>
2020-10-02 17:38:00 +0100

    Plot multiple files in sequence

--- ash_model_plotting/multiprocessing_experiments.py
+++ ash_model_plotting/multiprocessing_experiments.py
@@ -1,14 +1,32 @@
 """
 Messing around with multiprocessing
 """
+import logging
 from pathlib import Path
 
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
+PLOT_COUNT = 5
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def plot_many():
+    all_x = [range(x + 1) for x in range(PLOT_COUNT)]
+    all_y = [range(y + 1) for y in range(PLOT_COUNT)]
+    titles = [f'plot_{x}' for x in range(PLOT_COUNT)]
+    directory = Path('/tmp') / 'multi'
+    if not directory.is_dir():
+        directory.mkdir()
+
+    for i, title in enumerate(titles):
+        plot_fig(all_x[i], all_y[i], title, directory)
+
 
 def plot_fig(x, y, title, directory):
+    logging.debug("Plotting %s", title)
     fig, ax = plt.subplots()
     ax.plot(x, y)
     ax.set_title(title)
@@ -16,6 +34,4 @@ def plot_fig(x, y, title, directory):
 
 
 if __name__ == '__main__':
-    x = range(5)
-    y = range(5)
-    plot_fig(x, y, 'one_plot', '/tmp')
+    plot_many()
