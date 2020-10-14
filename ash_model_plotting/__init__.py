f8a2314031bfbf658aa421555f880ef0ee8a4d09
John A Stevenson <jostev@bgs.ac.uk>
2020-10-14 11:25:52 +0100

    Set matplotlib.backend to 'Agg'
    The Agg backend is required for plotting without a display. Setting it
    in the plotting code results in a warning as it must be set before
    matplotlib.pyplot is imported, and that happens when setting up Iris
    imports.  Setting in ash_model_plotting/__init__.py ensures that it is
    set before any other imports.

--- ash_model_plotting/__init__.py
+++ ash_model_plotting/__init__.py
@@ -1,5 +1,13 @@
 """Simplified plotting of dispersion model results."""
 # flake8: noqa
+# pylint: disable=wrong-import-position
+
+# Tell matplotlib to use the 'Agg' backend for plotting
+# We have to do this here, before importing Iris, as Iris
+# will set it to TkAgg by default.
+import matplotlib
+matplotlib.use('Agg')
+
 from ash_model_plotting.ash_model_results import (
     AshModelResult,
     NameAshModelResult,
